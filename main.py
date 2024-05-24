6e43f500795284d21dc69264fbc3139a0610d137
Chanathip Kulsirilak <mamcracker.mana@gmail.com>
2024-05-24 21:25:22 +0700

    add main

---
+++ main.py
@@ -0,0 +1,12 @@
+import sys
+
+sys.path.append("./src/")
+sys.path.append("./src/maths")
+
+from get_input import get_string_input
+from second_degree_polynomial import solve_equation as solve_2nd_polynomial
+
+program_count = 1
+
+print("Welcome to the EverydayTasksProgram!")
+print(f"There are currently {program_count} programs you can use.")
